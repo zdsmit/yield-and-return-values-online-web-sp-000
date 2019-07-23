e5c6700ce4290b0191eb72c9d2c19f854c629780
Zach  Smith <zdsmith001@gmail.com>
2019-07-23 14:51:55 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -2,9 +2,9 @@ require 'pry'
 
 def hello(array)
   i = 0
+  collection = []
   while i < array.length
-    binding.pry
-    yield(array[i])
+    collection << yield(array[i])
     i += 1
   end
 end
